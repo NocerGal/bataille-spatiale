3c3fc94a01521e073b2636015c60ef263a924434
NocerGal <luc.schenherr@hotmail.fr>
2024-06-16 19:08:48 +0200

    t

--- obj/Debug/net8.0/bataille.AssemblyInfo.cs
+++ obj/Debug/net8.0/bataille.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bataille")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f072edbb74a1a895ea03124a81ddc2f9af42670")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a03f8d60f48c56a7d85eaf1af95ebacbda8ff7f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("bataille")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bataille")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
